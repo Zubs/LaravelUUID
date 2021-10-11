fc3df1588dafd526dfc436b4772254754d460762
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2021-10-11 18:49:39 +0100

    With comments and shorthands

--- app/traits/UUID.php
+++ app/traits/UUID.php
@@ -8,19 +8,27 @@ trait UUID
 {
     protected static function boot ()
     {
+        // Boot other traits on the Model
         parent::boot();
+
+        /**
+         * Listen for the creating event on the user model.
+         * Sets the 'id' to a UUID using Str::uuid() on the instance being created
+         */
         static::creating(function ($model) {
-            if (empty($model->{$model->getKeyName()})) {
-                $model->{$model->getKeyName()} = Str::uuid()->toString();
+            if ($model->getKey() === null) {
+                $model->setAttribute($model->getKeyName(), Str::uuid()->toString());
             }
         });
     }
 
+    // Tells the database not to auto-increment this field
     public function getIncrementing ()
     {
         return false;
     }
 
+    // Helps the application specify the field type in the database
     public function getKeyType ()
     {
         return 'string';
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $users = User::all();
-
-    return view('welcome')->with('users', $users);
+    return view('welcome')->with('users', User::all());
 });
